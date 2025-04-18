5d72753ec84b787a4a084bc0c0d200ce198268cc
pedrogouveia77 <pedro.gouveiam7@gmail.com>
2025-04-18 19:12:48 -0300

    Atualiza versão da API do Stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,8 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2025-03-31.basil",
+  apiVersion: "2023-10-16",
   appInfo: {
     name: "Ignite Shop",
-  }
-});
+  },
+} as any);
